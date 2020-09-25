98dcd4d9dcdbb88005c24346aaf2a57a4fcb4b3f
xieguigang <xie.guigang@gcmodeller.org>
2020-09-26 00:53:59 +0800

    delete text

--- console/ConsoleControl.vb
+++ console/ConsoleControl.vb
@@ -1,10 +1,6 @@
 ﻿Public Class ConsoleControl
 
-    Dim console As Console
-
-    Private Sub RichTextBox1_TextChanged(sender As Object, e As EventArgs) Handles RichTextBox1.TextChanged
-
-    End Sub
+    Public ReadOnly Property Console As Console
 
     Private Sub RichTextBox1_KeyPress(sender As Object, e As KeyPressEventArgs) Handles RichTextBox1.KeyPress
         e.Handled = True
@@ -27,6 +23,7 @@
                 ' can not delete
                 e.Handled = False
             End If
+
         Else
             RichTextBox1.AppendText(e.KeyChar)
         End If
@@ -36,7 +33,7 @@
         RichTextBox1.Text = ""
         RichTextBox1.ReadOnly = True
 
-        console = New Console(RichTextBox1)
+        _Console = New Console(RichTextBox1)
     End Sub
 
     Private Sub RichTextBox1_KeyUp(sender As Object, e As KeyEventArgs) Handles RichTextBox1.KeyUp
@@ -51,6 +48,19 @@
         ElseIf e.KeyCode = Keys.Right Then
             If cursor < RichTextBox1.TextLength Then
                 RichTextBox1.Select(cursor + 1, 0)
+            End If
+        ElseIf e.KeyCode = Keys.Delete Then
+            RichTextBox1.Select(RichTextBox1.TextLength, 0)
+            Dim lastFirst = RichTextBox1.GetFirstCharIndexOfCurrentLine
+
+            If cursor > lastFirst Then
+                RichTextBox1.ReadOnly = False
+                RichTextBox1.Select(cursor, 1)
+                RichTextBox1.SelectedText = ""
+                RichTextBox1.ReadOnly = True
+            Else
+                ' can not delete
+                e.Handled = False
             End If
         End If
     End Sub
