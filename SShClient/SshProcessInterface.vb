3bfe6e1def26e0b4075bdd3d97671fc9019e099d
この中二病に爆焔を！ <xieguigang@metabolomics.ac.cn>
2026-08-02 05:45:01 +0800

    create a local shell model

--- SShClient/SshProcessInterface.vb
+++ SShClient/SshProcessInterface.vb
@@ -5,23 +5,6 @@ Imports Microsoft.VisualBasic.Windows.Forms.Win32
 Imports Renci.SshNet.Common
 Imports SSH = Renci.SshNet
 
-Public Class LocalShellInterface : Inherits AbstractProcessInterface
-
-    Public Sub New()
-        MyBase.New(Nothing)
-        ansi = True
-    End Sub
-
-    Public Overrides Sub WriteInput(input As String)
-        Call RaiseOutputEvent(New TextSpan(Environment.UserName & "@" & Environment.MachineName, AnsiColor.Green) & AnsiEscapeCodes.Reset & " ")
-        '  Call RaiseOutputEvent("$ " & AnsiEscapeCodes.GetMoveCursorUp(1))
-    End Sub
-
-    Public Overrides Sub WriteRaw(input As String)
-
-    End Sub
-End Class
-
 ''' <summary>
 ''' An <see cref="AbstractProcessInterface"/> implementation that drives a remote
 ''' shell over SSH using the SSH.NET library. The remote shell is attached to a
