66cd3fbdb888c5853aee6522d172e101a659dcca
この中二病に爆焔を！ <xieguigang@metabolomics.ac.cn>
2026-08-04 00:38:20 +0800

    Add WebView2 support and convert console project to a library
    Added a Microsoft.Web.WebView2 (1.0.4129.50) package reference to the SShClient, console, and test projects, introducing embedded web-browser capability that the UI can rely on for rendering rich content.
    
    Refactored console/console.NET5.vbproj from an executable WinForms app into a class library by setting OutputType to Library, switching MyType from WindowsFormsWithCustomSubMain to the standard WindowsForms model, and pointing StartupObject to My.MyApplication so the VB.NET application framework boots normally. UseWPF was also enabled so the library can host WPF/WinUI content alongside WebView2.
    
    Updated the application manifest (Application.myapp): the XML encoding was changed to utf-16 and the MainForm entry was cleared, which matches the new library configuration where no dedicated main form exists.
    
    These changes prepare the shared console library to host WebView2-based UI surfaces and allow dependent projects (e.g. test) to consume the same browser runtime.

---
+++ console/WebView2/WebViewConsole.Designer.vb
@@ -0,0 +1,54 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class WebViewConsole
+    Inherits System.Windows.Forms.UserControl
+
+    'UserControl overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        WebView21 = New Web.WebView2.WinForms.WebView2()
+        CType(WebView21, System.ComponentModel.ISupportInitialize).BeginInit()
+        SuspendLayout()
+        ' 
+        ' WebView21
+        ' 
+        WebView21.AllowExternalDrop = True
+        WebView21.CreationProperties = Nothing
+        WebView21.DefaultBackgroundColor = Color.White
+        WebView21.Dock = DockStyle.Fill
+        WebView21.Location = New Point(0, 0)
+        WebView21.Name = "WebView21"
+        WebView21.Size = New Size(597, 372)
+        WebView21.TabIndex = 0
+        WebView21.ZoomFactor = 1R
+        ' 
+        ' WebViewConsole
+        ' 
+        AutoScaleDimensions = New SizeF(7F, 15F)
+        AutoScaleMode = AutoScaleMode.Font
+        Controls.Add(WebView21)
+        Name = "WebViewConsole"
+        Size = New Size(597, 372)
+        CType(WebView21, System.ComponentModel.ISupportInitialize).EndInit()
+        ResumeLayout(False)
+    End Sub
+
+    Friend WithEvents WebView21 As Web.WebView2.WinForms.WebView2
+
+End Class
---
+++ console/WebView2/WebViewConsole.vb
@@ -0,0 +1,3 @@
+﻿Public Class WebViewConsole
+
+End Class
